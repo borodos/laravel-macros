1d11e781327ba1dd00a52b26a05ee81666acfbf8
Georgiy Malahov <stepllerrus78@gmail.com>
2024-11-29 15:21:56 +0300

    init

---
+++ src/Providers/BrdsMacrosServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Collection;
+use Illuminate\Support\ServiceProvider;
+
+class BrdsMacrosServiceProvider extends ServiceProvider {
+    /**
+     * Register any application services.
+     */
+    public function register(): void {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void {
+        Collection::macro('recursive', function () {
+            return $this->map(function ($value) {
+                if (is_array($value) || is_object($value)) {
+                    return collect($value)->recursive();
+                }
+
+                return $value;
+            });
+        });
+    }
+}
